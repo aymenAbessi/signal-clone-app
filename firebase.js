549dee1ab0defed8643bd42f0c8d5ecef83d0bea
aymenAbessi <99690345+aymenAbessi@users.noreply.github.com>
2024-05-28 17:24:10 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0U3t4amExpQWPg9LAqIm7GfapF27dO2Q",
-  authDomain: "signal-aecf1.firebaseapp.com",
-  projectId: "signal-aecf1",
-  storageBucket: "signal-aecf1.appspot.com",
-  messagingSenderId: "394481880606",
-  appId: "1:394481880606:web:f770b95fc5d469a617a80f",
-  measurementId: "G-19XFDQF9V6"
+
 };
 
 
